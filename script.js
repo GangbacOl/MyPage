53574393e4d458435eef0e2ecd67ba04e6621a57
GangbacOl <gangyeonjo336@gmail.com>
2020-09-17 05:13:50 +0900

    [UPD] 클릭 시 스크롤해주는 콜백 작성.

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ $(document).ready(function () {
             {
                 scrollTop: $('.container_3').offset().top,
             },
-            600
+            500
         );
     });
-    // $('.container_1').on('mousewheel', () => {
-    //     $('body').animate(
-    //         {
-    //             scrollTop: $('.container_2').offset().top,
-    //         },
-    //         500
-    //     );
-    // });
+    $('body > div.container_1 > div.header > div.nav > div > li:nth-child(3)').click(() => {
+        $('body').animate(
+            {
+                scrollTop: $('.container_4').offset().top,
+            },
+            500
+        );
+    });
 });
